32475b7a1dfaa652d24ebd5340e147fbc18c8d76
fernandoEncalada <ferencalada1@gmail.com>
2022-10-09 23:31:04 -0500

    pokemon options

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="fade-in">Who is that Pokemon?</h1>
+
+  <PokemonPicture :pokemonId="6" :showPokemon="true"/>
+
+  <PokemonOptions />
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue';
+import PokemonOptions from '@/components/PokemonOptions.vue';
+export default {
+    components: { PokemonPicture, PokemonOptions }
+}
+</script>
+
+<style>
+
+</style>
